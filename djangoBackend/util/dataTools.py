1c1e87f6eea01c9816378f0e2e7a8f3b769a90fa
lyx <765020742@qq.com>
2020-07-01 21:38:43 +0800

    新增先redis后db的数据获取

--- djangoBackend/util/dataTools.py
+++ djangoBackend/util/dataTools.py
@@ -4,21 +4,31 @@
 # @Author : LYX-夜光
 import json
 
+from django.core.serializers.json import DjangoJSONEncoder
 from django_redis import get_redis_connection
 
 class RedisData:
+    DATA_TIME = 60*60*24*30  # 30天
     redisConn = get_redis_connection()
 
+    @staticmethod  # 数据库数据key
+    def getDataKey(model, id):
+        return '/%s/%s' % (model, id)
+
+    @staticmethod  # 用户sn key
+    def getSnKey(userId, device):
+        return '/user/%s/sn/%s' % (userId, device)
+
     @staticmethod  # 在redis中获取单个数据
-    def getInfo(key, loads=True):
+    def getData(key, loads=True):
         try:
             results = RedisData.redisConn.get(key)
         except:
-            results = None
+            results = {}
         return results if not results or not loads else json.loads(results)
 
     @staticmethod  # 在redis中获取批量数据
-    def getInfoList(keyList):
+    def getDataList(keyList):
         try:
             redisConn = RedisData.redisConn.pipeline(transaction=False)
             for key in keyList:
@@ -29,10 +39,10 @@ class RedisData:
         return results
 
     @staticmethod  # 在redis中设置信息， key键， results值， time设置时间（单位：s）
-    def setInfo(key, results, time=-1, dumps=True):
+    def setData(key, results, time=-1, dumps=True):
         try:
             if dumps:
-                RedisData.redisConn.set(key, json.dumps(results))
+                RedisData.redisConn.set(key, json.dumps(results, cls=DjangoJSONEncoder))
             else:
                 RedisData.redisConn.set(key, results)
             if time > -1:
@@ -42,12 +52,12 @@ class RedisData:
             return False
 
     @staticmethod  # 在redis中批量设置信息， keyValue键值对， time设置时间（单位：s）
-    def setInfoList(keyValue, time=-1, dumps=True):
+    def setDataList(keyValue, time=-1, dumps=True):
         try:
             redisConn = RedisData.redisConn.pipeline(transaction=False)
             for key in keyValue:
                 if dumps:
-                    redisConn.set(key, json.dumps(keyValue[key]))
+                    redisConn.set(key, json.dumps(keyValue[key], cls=DjangoJSONEncoder))
                 else:
                     redisConn.set(key, keyValue[key])
                 if time > -1:
@@ -67,13 +77,17 @@ class RedisData:
         except:
             return False
 
-    @staticmethod  # 设置redis键的有效时间
-    def setTimeList(keyList, time):
-        try:
-            redisConn = RedisData.redisConn.pipeline(transaction=False)
-            for key in keyList:
-                redisConn.expire(key, time)
-            redisConn.execute()
-            return True
-        except:
-            return False
+class Data:
+    @staticmethod  # 先在redis后在db中获取数据
+    def getData(model, id):
+        modelName = model._meta.object_name
+        modelName = modelName[0].lower() + modelName[1:]
+        key = RedisData.getDataKey(modelName, id)
+        data = RedisData.getData(key)
+        if not data:
+            data = model.objects.filter(**{modelName+'Id': id}).values(*model.allowFields()).first()
+            if data:
+                RedisData.setData(key, data, RedisData.DATA_TIME)
+            else:
+                data = {}
+        return data
